feb77e1197255aa676bc009bc9619e2d00f70eb1
cindyflowers <cflowers@myattatech.com>
2019-12-04 15:32:46 -0500

    more cleanup

--- scripts/index.js
+++ scripts/index.js
@@ -149,22 +149,22 @@ $(function() {
   setTimeout(function() { onScrollInit($('.waypoint')) }, 10);
 
   // CONTACT FORM
-  $('#contact-form').submit(function(e) {
-    e.preventDefault();
+  // $('#contact-form').submit(function(e) {
+  //   e.preventDefault();
 
-      $.ajax({
-          url: "https://formspree.io/mnqnzzgv",
-          method: "POST",
-          data: { message: $('form').serialize() },
-          dataType: "json"
-      }).done(function(response) {
-          $('#success').addClass('expand');
-          $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
-      });
-  });
+  //     $.ajax({
+  //         url: "https://formspree.io/mnqnzzgv",
+  //         method: "POST",
+  //         data: { message: $('form').serialize() },
+  //         dataType: "json"
+  //     }).done(function(response) {
+  //         $('#success').addClass('expand');
+  //         $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
+  //     });
+  // });
 
-  $('#close').click(function() {
-    $('#success').removeClass('expand');
-  })
+  // $('#close').click(function() {
+  //   $('#success').removeClass('expand');
+  // })
 
 });
